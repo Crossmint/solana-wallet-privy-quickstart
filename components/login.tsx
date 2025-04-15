8dca63f2a9999d1bf6592b34531d2b86be8e905f
Agus <agustin@paella.dev>
2025-04-15 05:29:48 -0400

    Update login.tsx

--- components/login.tsx
+++ components/login.tsx
@@ -10,7 +10,7 @@ export function LoginButton() {
       className="w-full py-2 px-4 rounded-md text-sm font-medium border bg-gray-50 hover:bg-gray-100 transition-colors"
       onClick={login}
     >
-      Connect wallet
+      Log in or sign up
     </button>
   );
 }
